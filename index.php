3c119a9d1d854788c951e6cab71353516ea6e8e1
Mahnoormalik123 <154795582+Mahnoormalik123@users.noreply.github.com>
2024-01-31 15:01:41 +0500

    index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>csssss</title>
+  <title>wordpress</title>
 </head>
 <body>
   <?php get_header(); ?>
