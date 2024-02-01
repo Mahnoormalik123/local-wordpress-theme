3e872db8e05951f3dc4c04260b28f72fd8c9f0c5
Mahnoormalik123 <154795582+Mahnoormalik123@users.noreply.github.com>
2024-02-01 18:10:26 +0500

    single.php

--- single.php
+++ single.php
@@ -1,29 +1,16 @@
-<?php
- 
-get_header(); ?>
+?php get_header(); ?>
  
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
  
-        <?php
-        while ( have_posts() ) : the_post();
-            
-            get_template_part( 'content', get_post_format() );
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
-            the_post_navigation( array(
-                'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="post-title">%title</span>',
-                'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="post-title">%title</span>',
-            ) );
+        <?php 
+        while ( have_posts() ) : the_post(); ?>
+        <h1> <?php the_title(); ?> </h1> <?php
+        the_content();    
         endwhile;
         ?>
  
-        </main><!-- .site-main -->
-    </div><!-- .content-area -->
+        </main>
+    </div>
  
 <?php get_footer(); ?>
