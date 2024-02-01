8e95be22648746d2a006fe4c1eb90c316bfbd760
Mahnoormalik123 <154795582+Mahnoormalik123@users.noreply.github.com>
2024-02-01 15:21:50 +0500

    functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,11 @@
 <?php
 
-function wpdocs_theme_name_scripts() {
-	wp_enqueue_style( 'stylecss', get_stylesheet_uri());
+function firsttheme_files() {
+	wp_enqueue_style('main-styling', get_stylesheet_uri() );
+	wp_enqueue_script('main-js',get_template_directory_uri(). '/js/index.js',array(),'1.1',true);
 }
-add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' )
+add_action( 'wp_enqueue_script', 'firsttheme_files');
 
+register_nav_menus(array('firsttheme_mainmenu'=>'main menu', 'firsttheme_footermenu' => 'footer menu' ));
 
 ?>
