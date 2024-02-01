c1c37f19747d4f019dd88308ad15f075f41bad76
Mahnoormalik123 <154795582+Mahnoormalik123@users.noreply.github.com>
2024-02-01 15:20:54 +0500

    index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>wordpress</title>
+  <title>css</title>
 </head>
+<?php wp_nav_menu(array ('theme_location'=>'firsttheme_mainmenu','depth'=>2)); ?>
 <body>
+
   <?php get_header(); ?>
 
   <section>
@@ -34,3 +35,4 @@
 
 </body>
 </html>
+
