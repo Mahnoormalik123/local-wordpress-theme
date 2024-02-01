26d88e1fdc3cc06cfe9eed3f053978428d404871
Mahnoormalik123 <154795582+Mahnoormalik123@users.noreply.github.com>
2024-02-01 18:06:52 +0500

    functions.php

--- functions.php
+++ functions.php
@@ -1,11 +1,26 @@
 <?php
 
-function firsttheme_files() {
-	wp_enqueue_style('main-styling', get_stylesheet_uri() );
-	wp_enqueue_script('main-js',get_template_directory_uri(). '/js/index.js',array(),'1.1',true);
+function wpdocs_theme_name_scripts() {
+	wp_enqueue_style( 'firstWebsitestyles', get_stylesheet_uri() );
+	// wp_enqueue_scripts("index", get_template_directory_uri() . "/js/index.js");
+	
 }
-add_action( 'wp_enqueue_script', 'firsttheme_files');
+add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
 
-register_nav_menus(array('firsttheme_mainmenu'=>'main menu', 'firsttheme_footermenu' => 'footer menu' ));
 
+function custom_theme_navigation_menus() {
+    register_nav_menus(
+        array(
+            'main-menu' => __( 'Main Menu' ),
+        )
+    );
+}
+
+add_action( 'init', 'custom_theme_navigation_menus' );
+
+
+function enqueue_custom_scripts() {
+    // Enqueue your custom JS file
+    wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/index.js', array('jquery'), '1.0', true);
+}
 ?>
