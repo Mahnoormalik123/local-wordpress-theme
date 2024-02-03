dad987d708866c4550c9fc7fa547267bb980801e
Mahnoormalik123 <154795582+Mahnoormalik123@users.noreply.github.com>
2024-02-03 17:04:25 +0500

    functions.php

--- functions.php
+++ functions.php
@@ -23,4 +23,30 @@ function enqueue_custom_scripts() {
     // Enqueue your custom JS file
     wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/index.js', array('jquery'), '1.0', true);
 }
+function add_header_theme_support(){
+	$args = array(
+		'width'              => 1000,
+		'height'             => 250,
+		'flex-width'         => true,
+		'flex-height'        => true,
+	);
+	add_theme_support( 'custom-header', $args );
+}
+add_action('after_setup_theme','add_header_theme_support');
+add_action('wp_enqueue_scripts','enqueue_custom_scripts');
+add_action('widgets_init','firstwebsite_widgets_init');
+
+
+function firstwebsite_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Primary Sidebar', 'theme_name' ),
+		'id'            => 'sidebar-1',
+	) );
+	register_sidebar( array(
+		'name'          => __( 'Secondary Sidebar', 'theme_name' ),
+		'id'            => 'sidebar-2',
+	) );
+add_theme_support('custom-header');
+    
+}
 ?>
